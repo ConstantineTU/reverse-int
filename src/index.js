6f4b237c69036a6c512e918461533c08c59f5001
ConstantineTU <8kozmo8@gmail.com>
2021-08-17 22:13:10 +0500

    init: task compete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+	let nStr = String(n)
+	let reverseNStr = []
+	let reverseN = ''
+	for (let i = 0; i < nStr.length; i++) {
+		if (nStr[i] !== '-') {
+			reverseNStr.unshift(nStr[i])
+		}
+	}
+	for (let i = 0; i < reverseNStr.length; i++) {
+		if (reverseNStr[reverseNStr.length - 1] !== '0') {
+			reverseN += reverseNStr[i]
+		}
+	}
+	reverseN = Number(reverseN)
+	return reverseN
 }
